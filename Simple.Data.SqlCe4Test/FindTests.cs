f167b31b40d799cc32d39f56efbd24e321342d51
markrendle <mark@markrendle.net>
2010-08-06 12:19:00 +0100

    Fixed test to run in VS and R#

--- Simple.Data.SqlCe4Test/FindTests.cs
+++ Simple.Data.SqlCe4Test/FindTests.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Reflection;
 using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Simple.Data.SqlCe;
+using System.IO;
 
-namespace Simple.Data.SqlCe4Test
+namespace Simple.Data.SqlCeTest
 {
     /// <summary>
     /// Summary description for FindTests
@@ -14,19 +16,24 @@ namespace Simple.Data.SqlCe4Test
     [TestClass]
     public class FindTests
     {
+        private static readonly string DatabasePath = Path.Combine(
+            Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase.Substring(8)),
+            "TestDatabase.sdf");
+
         [TestMethod]
         public void TestMethod1()
         {
             Trace.WriteLine(Environment.CurrentDirectory);
-            var provider = ProviderHelper.GetProviderByFilename(@"..\..\..\TestDatabase.sdf");
+            var provider = ProviderHelper.GetProviderByFilename(DatabasePath);
             Assert.IsInstanceOfType(provider, typeof(SqlCeProvider));
         }
 
         [TestMethod]
         public void TestFindById()
         {
+            Trace.WriteLine(Assembly.GetExecutingAssembly().CodeBase);
             Trace.WriteLine(Environment.CurrentDirectory);
-            var db = Database.OpenFile(@"..\..\..\TestDatabase.sdf");
+            var db = Database.OpenFile(DatabasePath);
             var user = db.Users.FindById(1);
             Assert.AreEqual(1, user.Id);
         }
