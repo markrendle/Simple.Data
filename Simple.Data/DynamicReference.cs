639c3498d6104d1b2697654555813e1b89b55f6e
Mark Rendle <mark@markrendle.net>
2010-10-30 02:17:55 +0100

    Added doc comments to DynamicReference

--- Simple.Data/DynamicReference.cs
+++ Simple.Data/DynamicReference.cs
@@ -7,6 +7,9 @@ using System.Text;
 
 namespace Simple.Data
 {
+    /// <summary>
+    /// Represents a qualified reference to a data store object, such as a table or column.
+    /// </summary>
     public class DynamicReference : DynamicObject, IEquatable<DynamicReference>
     {
         private readonly string _name;
@@ -22,63 +25,119 @@ namespace Simple.Data
             _owner = owner;
         }
 
+        /// <summary>
+        /// Gets the owner of the referenced object; <c>null</c> if the object is top-level.
+        /// </summary>
+        /// <value>The owner.</value>
         public DynamicReference Owner
         {
             get { return _owner; }
         }
 
+        /// <summary>
+        /// Gets the name of the referenced object.
+        /// </summary>
+        /// <value>The name.</value>
         public string Name
         {
             get { return _name; }
         }
 
+        /// <summary>
+        /// Provides the implementation for operations that get member values. Classes derived from the <see cref="T:System.Dynamic.DynamicObject"/> class can override this method to specify dynamic behavior for operations such as getting a value for a property.
+        /// </summary>
+        /// <param name="binder">Provides information about the object that called the dynamic operation. The binder.Name property provides the name of the member on which the dynamic operation is performed. For example, for the Console.WriteLine(sampleObject.SampleProperty) statement, where sampleObject is an instance of the class derived from the <see cref="T:System.Dynamic.DynamicObject"/> class, binder.Name returns "SampleProperty". The binder.IgnoreCase property specifies whether the member name is case-sensitive.</param>
+        /// <param name="result">The result of the get operation. For example, if the method is called for a property, you can assign the property value to <paramref name="result"/>.</param>
+        /// <returns>
+        /// true if the operation is successful; otherwise, false. If this method returns false, the run-time binder of the language determines the behavior. (In most cases, a run-time exception is thrown.)
+        /// </returns>
         public override bool TryGetMember(GetMemberBinder binder, out object result)
         {
             result = new DynamicReference(binder.Name, this);
             return true;
         }
 
+        /// <summary>
+        /// Gets the names of all objects included in the reference as an array, with the uppermost object first.
+        /// </summary>
+        /// <returns></returns>
         public string[] GetAllObjectNames()
         {
             if (ReferenceEquals(Owner, null)) return new[] {_name};
             return _owner.GetAllObjectNames().Concat(new[] {_name}).ToArray();
         }
 
-        public static DynamicReference FromString(string source)
+        internal static DynamicReference FromString(string source)
         {
             return FromStrings(source.Split('.'));
         }
 
-        public static DynamicReference FromStrings(params string[] source)
+        internal static DynamicReference FromStrings(params string[] source)
         {
             return source.Aggregate<string, DynamicReference>(null, (current, element) => new DynamicReference(element, current));
         }
 
+        /// <summary>
+        /// Implements the operator == to create a <see cref="SimpleExpression"/> with the type <see cref="SimpleExpressionType.Equal"/>.
+        /// </summary>
+        /// <param name="column">The column.</param>
+        /// <param name="value">The value.</param>
+        /// <returns>The expression.</returns>
         public static SimpleExpression operator ==(DynamicReference column, object value)
         {
             return new SimpleExpression(column, value, SimpleExpressionType.Equal);
         }
 
+        /// <summary>
+        /// Implements the operator != to create a <see cref="SimpleExpression"/> with the type <see cref="SimpleExpressionType.NotEqual"/>.
+        /// </summary>
+        /// <param name="column">The column.</param>
+        /// <param name="value">The value.</param>
+        /// <returns>The expression.</returns>
         public static SimpleExpression operator !=(DynamicReference column, object value)
         {
             return new SimpleExpression(column, value, SimpleExpressionType.NotEqual);
         }
 
+        /// <summary>
+        /// Implements the operator &lt; to create a <see cref="SimpleExpression"/> with the type <see cref="SimpleExpressionType.LessThan"/>.
+        /// </summary>
+        /// <param name="column">The column.</param>
+        /// <param name="value">The value.</param>
+        /// <returns>The expression.</returns>
         public static SimpleExpression operator <(DynamicReference column, object value)
         {
             return new SimpleExpression(column, value, SimpleExpressionType.LessThan);
         }
 
+        /// <summary>
+        /// Implements the operator &gt; to create a <see cref="SimpleExpression"/> with the type <see cref="SimpleExpressionType.GreaterThan"/>.
+        /// </summary>
+        /// <param name="column">The column.</param>
+        /// <param name="value">The value.</param>
+        /// <returns>The expression.</returns>
         public static SimpleExpression operator >(DynamicReference column, object value)
         {
             return new SimpleExpression(column, value, SimpleExpressionType.GreaterThan);
         }
 
+        /// <summary>
+        /// Implements the operator &lt;= to create a <see cref="SimpleExpression"/> with the type <see cref="SimpleExpressionType.LessThanOrEqual"/>.
+        /// </summary>
+        /// <param name="column">The column.</param>
+        /// <param name="value">The value.</param>
+        /// <returns>The expression.</returns>
         public static SimpleExpression operator <=(DynamicReference column, object value)
         {
             return new SimpleExpression(column, value, SimpleExpressionType.LessThanOrEqual);
         }
 
+        /// <summary>
+        /// Implements the operator &gt;= to create a <see cref="SimpleExpression"/> with the type <see cref="SimpleExpressionType.GreaterThanOrEqual"/>.
+        /// </summary>
+        /// <param name="column">The column.</param>
+        /// <param name="value">The value.</param>
+        /// <returns>The expression.</returns>
         public static SimpleExpression operator >=(DynamicReference column, object value)
         {
             return new SimpleExpression(column, value, SimpleExpressionType.GreaterThanOrEqual);
