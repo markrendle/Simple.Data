4de289f51200c0612ab4e8e2e2219cea4aa12481
markrendle <mark@markrendle.net>
2010-12-17 15:37:49 +0000

    Version to 0.3.2.2

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -19,5 +19,5 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.3.2.1")]
-[assembly: AssemblyFileVersion("0.3.2.1")]
+[assembly: AssemblyVersion("0.3.2.2")]
+[assembly: AssemblyFileVersion("0.3.2.2")]
