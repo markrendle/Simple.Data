efe0c1977210dc3cf18219b794bdc8ec81814a6d
markrendle <mark@markrendle.net>
2011-01-11 16:40:17 +0000

    Junk

--- Releases/CopyReleases.cmd
+++ Releases/CopyReleases.cmd
@@ -1,8 +1,12 @@
 cd %1\Releases
 set reldir=%DATE:~-4%%DATE:~3,2%%DATE:~0,2%%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%%TIME:~9,2%
 mkdir %reldir%
-copy ..\Simple.Data\bin\Release\*.dll %reldir%
-copy ..\Simple.Data\bin\Release\*.pdb %reldir%
+copy ..\Simple.Data\bin\Release\Simple.Data.dll %reldir%
+copy ..\Simple.Data\bin\Release\Simple.Data.pdb %reldir%
+copy ..\Simple.Data.AdapterApi\bin\Release\Simple.Data.AdapterApi.dll %reldir%
+copy ..\Simple.Data.AdapterApi\bin\Release\Simple.Data.AdapterApi.pdb %reldir%
+copy ..\Simple.Data.Ado\bin\Release\Simple.Data.Ado.dll %reldir%
+copy ..\Simple.Data.Ado\bin\Release\Simple.Data.Ado.pdb %reldir%
 copy ..\Simple.Data.SqlServer\bin\Release\Simple.Data.SqlServer.dll %reldir%
 copy ..\Simple.Data.SqlServer\bin\Release\Simple.Data.SqlServer.pdb %reldir%
 copy ..\Simple.Data.SqlCe35\bin\Release\Simple.Data.SqlCe35.dll %reldir%
