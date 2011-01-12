9af7a59b7196e8ee48920669a90b93d84b0ad324
markrendle <mark@markrendle.net>
2011-01-12 17:33:05 +0000

    Added Function support within Simple bit

---
+++ Simple.Data/AdapterWithFunctionsExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Simple.Data.Commands;
+
+namespace Simple.Data
+{
+    internal static class AdapterWithFunctionsExtensions
+    {
+        public static bool IsValidFunction(this Adapter adapter, string functionName)
+        {
+            var adapterWithFunctions = adapter as IAdapterWithFunctions;
+            if (adapterWithFunctions == null) return false;
+            return adapterWithFunctions.IsValidFunction(functionName);
+        }
+
+        public static bool Execute(this Adapter adapter, string functionName, IEnumerable<string> parameterNames, IEnumerable<object> parameterValues, out object result)
+        {
+            var adapterWithFunctions = adapter as IAdapterWithFunctions;
+            if (adapterWithFunctions == null) throw new NotSupportedException("Adapter does not support Function calls.");
+            var command = new ExecuteFunctionCommand(adapterWithFunctions, functionName,
+                                                                     parameterNames, parameterValues);
+            return command.Execute(out result);
+        }
+    }
+}
--- Simple.Data/Commands/ExecuteFunctionCommand.cs
+++ Simple.Data/Commands/ExecuteFunctionCommand.cs
@@ -10,13 +10,9 @@ namespace Simple.Data.Commands
     {
         private readonly IAdapterWithFunctions _adapter;
         private readonly string _functionName;
+        private readonly FunctionReturnType _returnType;
         private readonly IEnumerable<string> _argumentNames;
         private readonly IEnumerable<object> _arguments;
-
-        public ExecuteFunctionCommand(IAdapterWithFunctions adapter)
-        {
-            _adapter = adapter;
-        }
 
         public ExecuteFunctionCommand(IAdapterWithFunctions adapter, string functionName, IEnumerable<string> argumentNames, IEnumerable<object> arguments)
         {
@@ -24,14 +20,60 @@ namespace Simple.Data.Commands
             _functionName = functionName;
             _argumentNames = argumentNames;
             _arguments = arguments;
+            _returnType = _adapter.GetReturnType(_functionName);
         }
 
         public bool Execute(out object result)
         {
             var resultType = _adapter.GetReturnType(_functionName);
             var func = GetFunc(resultType);
-            result = func(_functionName, MakeArguments());
+            result = ConvertToSimpleTypes(func(_functionName, MakeArguments()));
             return true;
+        }
+
+        public FunctionReturnType ReturnType
+        {
+            get
+            {
+                return _returnType;
+            }
+        }
+
+        private object ConvertToSimpleTypes(object source)
+        {
+            switch (_returnType)
+            {
+                case FunctionReturnType.ResultSet:
+                    return ToResultSet(source);
+                case FunctionReturnType.MultipleResultSets:
+                    return ToMultipleResultSets(source);
+                default:
+                    return source;
+            }
+        }
+
+        private static IEnumerable<DynamicEnumerable> ToMultipleResultSets(object source)
+        {
+            if (source == null) return Enumerable.Empty<DynamicEnumerable>();
+            var resultSets = source as IEnumerable<IEnumerable<IEnumerable<KeyValuePair<string, object>>>>;
+            if (resultSets == null) throw new InvalidOperationException("Adapter returned incorrect Type.");
+
+            return ToMultipleDynamicEnumerables(resultSets);
+        }
+
+        private static IEnumerable<DynamicEnumerable> ToMultipleDynamicEnumerables(IEnumerable<IEnumerable<IEnumerable<KeyValuePair<string, object>>>> resultSets)
+        {
+            return resultSets.Select(resultSet => new DynamicEnumerable(resultSet.Select(dict => new DynamicRecord(dict))));
+        }
+
+        private static DynamicEnumerable ToResultSet(object source)
+        {
+            if (source == null) return new DynamicEnumerable(Enumerable.Empty<dynamic>());
+
+            var dicts = source as IEnumerable<IEnumerable<KeyValuePair<string, object>>>;
+            if (dicts == null) throw new InvalidOperationException("Adapter returned incorrect Type.");
+
+            return new DynamicEnumerable(dicts.Select(dict => new DynamicRecord(dict)));
         }
 
         private Func<string, IEnumerable<KeyValuePair<string, object>>, object> GetFunc(FunctionReturnType resultType)
