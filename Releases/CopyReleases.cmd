a83021a85e97f75d4c35360cb31de363a2550eb4
ahazelwood <ahazelwood@ciradar.com>
2011-02-23 12:55:37 -0500

    Fixed Date formatting issues.

--- Releases/CopyReleases.cmd
+++ Releases/CopyReleases.cmd
@@ -1,5 +1,6 @@
+@echo off
 cd %1\Releases
-set reldir=%DATE:~-4%%DATE:~3,2%%DATE:~0,2%%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%%TIME:~9,2%
+set reldir="%DATE:~-4%%DATE:~4,2%%DATE:~7,2%%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%%TIME:~9,2%"
 mkdir %reldir%
 copy ..\Simple.Data\bin\Release\Simple.Data.dll %reldir%
 copy ..\Simple.Data\bin\Release\Simple.Data.pdb %reldir%
@@ -7,6 +8,8 @@ copy ..\Simple.Data.AdapterApi\bin\Release\Simple.Data.AdapterApi.dll %reldir%
 copy ..\Simple.Data.AdapterApi\bin\Release\Simple.Data.AdapterApi.pdb %reldir%
 copy ..\Simple.Data.Ado\bin\Release\Simple.Data.Ado.dll %reldir%
 copy ..\Simple.Data.Ado\bin\Release\Simple.Data.Ado.pdb %reldir%
+copy ..\Simple.Data.MongoDb\bin\Release\*.dll %reldir%
+copy ..\Simple.Data.MongoDb\bin\Release\Simple.Data.MongoDb.pdb %reldir%
 copy ..\Simple.Data.SqlServer\bin\Release\Simple.Data.SqlServer.dll %reldir%
 copy ..\Simple.Data.SqlServer\bin\Release\Simple.Data.SqlServer.pdb %reldir%
 copy ..\Simple.Data.SqlCe35\bin\Release\Simple.Data.SqlCe35.dll %reldir%
