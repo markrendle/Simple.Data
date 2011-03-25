a8bdd718875be4d16e3c23442c1c2c3bef86b1c0
Mark Rendle <mark@markrendle.net>
2011-03-25 00:23:01 +0000

    0.5.5 release

---
+++ NuGet/NuGetPush.cmd
@@ -0,0 +1,4 @@
+nuget push Simple.Data.Ado.0.5.5.nupkg 204d04e8-f42f-4061-bd5e-3dfcfd105b5a
+nuget push Simple.Data.SqlServer.0.5.5.nupkg 204d04e8-f42f-4061-bd5e-3dfcfd105b5a
+nuget push Simple.Data.SqlCompact40.0.5.5.nupkg 204d04e8-f42f-4061-bd5e-3dfcfd105b5a
+nuget push Simple.Data.Mocking.0.5.5.nupkg 204d04e8-f42f-4061-bd5e-3dfcfd105b5a
