cb233cf406d2c5ea513480d25a8902dc39425798
markrendle <mark@markrendle.net>
2011-07-27 13:48:55 +0100

    v0.8.1 release

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -19,5 +19,5 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.8.0.0")]
-[assembly: AssemblyFileVersion("0.8.0.0")]
+[assembly: AssemblyVersion("0.8.1.0")]
+[assembly: AssemblyFileVersion("0.8.1.0")]
