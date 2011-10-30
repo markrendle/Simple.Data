67f932146df34c1ce1c14a651a2b8009cd690729
Richard Hopton <richard@thehoptons.co.uk>
2011-10-30 09:39:11 +0000

    106: ParameterTemplates of Type "Other" break Parameter type inference

--- Simple.Data.Ado/IDbParameterFactory.cs
+++ Simple.Data.Ado/IDbParameterFactory.cs
@@ -6,6 +6,7 @@
 
     public interface IDbParameterFactory
     {
+        IDbDataParameter CreateParameter(string name);
         IDbDataParameter CreateParameter(string name, Column column);
         IDbDataParameter CreateParameter(string name, DbType dbType, int maxLength);
     }
