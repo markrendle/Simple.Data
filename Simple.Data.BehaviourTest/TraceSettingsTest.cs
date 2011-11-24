773ef6687bdfcc28edf359b9e2070221ee24dde1
markrendle <mark@markrendle.net>
2011-11-24 00:14:45 +0000

    Updated nuspec files to 0.11

---
+++ Simple.Data.BehaviourTest/TraceSettingsTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Simple.Data.IntegrationTest
+{
+    [TestFixture]
+    public class TraceSettingsTest
+    {
+        [Test]
+        public void TraceLevelShouldBePickedUpFromConfig()
+        {
+            Assert.AreEqual(TraceLevel.Error, Database.TraceLevel);
+        }
+    }
+}
