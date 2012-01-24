1bb7f841b06b0681d04b6294bae307a73824f25b
Richard Hopton <richard@thehoptons.co.uk>
2012-01-24 22:44:06 +0000

    Rework decimal tests

--- Simple.Data.SqlTest/SchemaTests/DataTypeTests.cs
+++ Simple.Data.SqlTest/SchemaTests/DataTypeTests.cs
@@ -12,16 +12,17 @@ namespace Simple.Data.SqlTest.SchemaTests
             var db = DatabaseHelper.Open();
             var value = db.DecimalTest.FindById(1).Value;
             Assert.AreEqual(typeof(Decimal), value.GetType());
-            Assert.AreEqual(1.234568, value);
+            Assert.AreEqual(1.234567, value);
         }
 
         [Test]
         public void TestDecimalCanBeInsertedCorrectly()
         {
             var db = DatabaseHelper.Open();
-            var value = db.DecimalTest.Insert(new { Value = 1.234567 }).Value;
+            var decimalTest = new { Value = 12.345678 };
+            var value = db.DecimalTest.Insert(decimalTest).Value;
             Assert.AreEqual(typeof(Decimal), value.GetType());
-            Assert.AreEqual(1.234567, value);
+            Assert.AreEqual(decimalTest.Value, value);
         }
     }
 }
