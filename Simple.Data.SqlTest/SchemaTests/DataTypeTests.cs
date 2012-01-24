d9d2c6d7345ce1eb5b93c685fb3edda22cb85c3b
Richard Hopton <richard@thehoptons.co.uk>
2012-01-24 21:39:39 +0000

    Add tests for Decimal data type

---
+++ Simple.Data.SqlTest/SchemaTests/DataTypeTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Simple.Data.SqlTest.SchemaTests
+{
+    [TestFixture]
+    public class DataTypeTests
+    {
+        [Test]
+        public void TestDecimalCanBeRetrievedCorrectly()
+        {
+            var db = DatabaseHelper.Open();
+            var value = db.DecimalTest.FindById(1).Value;
+            Assert.AreEqual(typeof(Decimal), value.GetType());
+            Assert.AreEqual(1.234568, value);
+        }
+
+        [Test]
+        public void TestDecimalCanBeInsertedCorrectly()
+        {
+            var db = DatabaseHelper.Open();
+            var value = db.DecimalTest.Insert(new { Value = 1.234567 }).Value;
+            Assert.AreEqual(typeof(Decimal), value.GetType());
+            Assert.AreEqual(1.234567, value);
+        }
+    }
+}
