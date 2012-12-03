927cc4ccdcd0cb425f2a8414f1ee04803635157b
markrendle <mark@markrendle.net>
2012-12-03 19:41:39 +0000

    NuGet 0.18.3.1

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -19,6 +19,6 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.18.3.0")]
-[assembly: AssemblyFileVersion("0.18.3.0")]
+[assembly: AssemblyVersion("0.18.3.1")]
+[assembly: AssemblyFileVersion("0.18.3.1")]
 
